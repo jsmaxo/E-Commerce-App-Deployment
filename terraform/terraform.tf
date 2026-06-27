5b468d178c3c02c0b6a9475f41d88f5e1b0340e0
jsmaxo <nanayakkaraj210@gmail.com>
2026-06-27 13:34:41 +0530

    updated files

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-s3-backend-901970227725"
+    bucket = "terraform-s3-backendjs"
     key    = "backend-locking"
     region = "eu-west-1"
     use_lockfile = true
