867200f79b67675cf5af50e23009b12dd8945707
jsmaxo <nanayakkaraj210@gmail.com>
2026-06-28 21:02:33 +0530

    update s3 name

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-s3-backendjs"
+    bucket = "terraform-ecom-s3-023703348895"
     key    = "backend-locking"
     region = "eu-west-1"
     use_lockfile = true
